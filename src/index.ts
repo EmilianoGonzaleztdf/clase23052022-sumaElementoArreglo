1392ffa37317e1a3349a9c7b9b2e5212d45b74bf
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-29 15:32:51 -0300

    funciona no entiendo bien que hice en el FOR que suma los valores, pero funciona. no me siento conforme. lo tengo que consultar con  Jose

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 let nArray: number = Number(prompt("ingrese la dimension del array"));
 let numberArray: number[] = new Array(nArray);
 let i: number;
+let resultado: number = 0; //inicializo resultado en valor 0 para poder usarlo matematicamente despues :D
 
 for (i = 0; i < nArray; i++) {
   numberArray[i] = Number(
     prompt("ingrese el valor numerico asignado a la posicion " + i)
   );
-}
-
+} //ingreso la longitud del array
+console.log("la longitud del array es: " + nArray);
 for (i = 0; i < nArray; i++) {
-  console.log("los valores cargados al array son: " + numberArray[i]);
-}
+  console.log(
+    "usted ingreso el valor " + numberArray[i],
+    "en la posicion " + i
+  );
+} //ingreso los valores del array
+for (i = 0; i < nArray; i++) {
+  let suma = numberArray[i] + resultado;
+  let resultadosuma = suma;
+  resultado = resultadosuma;
+} //sumo los valores
+console.log("la suma de los valores ingresados al array es: " + resultado);
